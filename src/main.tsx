f192575d99e4b6c71a618bb85afb3b89ea7b1d49
matchoo77 <matttopsecretemail@gmail.com>
2025-08-18 09:52:29 -0400

    Fix useState import error

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react';
+import React, { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
